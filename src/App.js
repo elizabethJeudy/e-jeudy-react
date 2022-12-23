72f39c51380155fba70d8a3d6bb815ecc78b3520
Elizabeth <tjeudy.e@gmail.com>
2022-12-23 16:24:22 -0500

    added components to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Projects from "./components/Projects";
 
 export default function App() {
-	return <div className="App"></div>;
+	return (
+		<div>
+			<Header />
+			<Nav />
+			<About />
+			<Projects />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
