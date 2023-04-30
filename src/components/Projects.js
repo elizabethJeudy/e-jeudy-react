c4e35ff850a264c7785a5a2348dcafc3965ab2cb
Elizabeth <tjeudy.e@gmail.com>
2023-04-30 15:25:11 -0400

    imported wordle to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Projects.css";
 import weatherappreact from "../images/weatherappreact.jpg";
 import dictionary from "../images/dictionary.png";
+import wordle from "../images/wordle.png";
 import CTA from "./CTA";
 
 export default function Projects() {
@@ -11,12 +12,12 @@ export default function Projects() {
 			<div className="container projectsContainer">
 				<article className="project-item">
 					<div className="project-img">
-						<img src={weatherappreact} alt=""></img>
+						<img src={wordle} alt=""></img>
 					</div>
-					<h3>Weather App React</h3>
+					<h3>Wordle Clone</h3>
 					<div className="ctaBtns">
 						<a
-							href="https://github.com/elizabethJeudy/weather-app-react"
+							href="https://github.com/elizabethJeudy/wordleClone"
 							className="btn"
 							target="_blank"
 							rel="noreferrer"
@@ -24,7 +25,7 @@ export default function Projects() {
 							Github
 						</a>
 						<a
-							href="https://ejeudy-weather-react.netlify.app/"
+							href="https://elizabethjeudy.github.io/wordleClone/"
 							className="btn btn-primary"
 							target="_blank"
 							rel="noreferrer"
@@ -50,6 +51,31 @@ export default function Projects() {
 						</a>
 						<a
 							href="https://dict-app-react.netlify.app/"
+							className="btn btn-primary"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Demo
+						</a>
+					</div>
+				</article>
+
+				<article className="project-item">
+					<div className="project-img">
+						<img src={weatherappreact} alt=""></img>
+					</div>
+					<h3>Weather App React</h3>
+					<div className="ctaBtns">
+						<a
+							href="https://github.com/elizabethJeudy/weather-app-react"
+							className="btn"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Github
+						</a>
+						<a
+							href="https://ejeudy-weather-react.netlify.app/"
 							className="btn btn-primary"
 							target="_blank"
 							rel="noreferrer"
