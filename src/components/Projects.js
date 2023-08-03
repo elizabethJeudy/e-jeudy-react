6d002788763425de39bf8e708298d69db31a6328
Elizabeth <tjeudy.e@gmail.com>
2023-08-03 16:56:46 -0400

    added aqi checker

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import "./Projects.css";
 // import weatherappreact from "../images/weatherappreact.jpg";
 import dictionary from "../images/dictionary.png";
 import wordle from "../images/wordle.png";
+import AQIchecker from "../images/AQIchecker.jpg";
 import CTA from "./CTA";
 
 export default function Projects() {
@@ -10,6 +11,34 @@ export default function Projects() {
 		<section id="projects">
 			<h2>Projects</h2>
 			<div className="container projectsContainer">
+				{/* AQI checker */}
+				<article className="project-item">
+					<div className="project-img">
+						<img src={AQIchecker} alt=""></img>
+					</div>
+					<h3>AQI Levels Checker</h3>
+					<h4>React.js, Node.js, API, Bootstrap</h4>
+					<div className="ctaBtns">
+						<a
+							href="https://github.com/elizabethJeudy/air-quality-checker"
+							className="btn"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Github
+						</a>
+						<a
+							href="https://elizabethjeudy.github.io/air-quality-checker/"
+							className="btn btn-primary"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Demo
+						</a>
+					</div>
+				</article>
+
+				{/* wordle clone */}
 				<article className="project-item">
 					<div className="project-img">
 						<img src={wordle} alt=""></img>
@@ -36,12 +65,13 @@ export default function Projects() {
 					</div>
 				</article>
 
+				{/* dictionary app */}
 				<article className="project-item">
 					<div className="project-img">
 						<img src={dictionary} alt=""></img>
 					</div>
 					<h3>Dictionary App</h3>
-					<h4> React.js, Node.js, Axios</h4>
+					<h4> React.js, Node.js, Axios, API</h4>
 					<div className="ctaBtns">
 						<a
 							href="https://github.com/elizabethJeudy/dictionary-app"
